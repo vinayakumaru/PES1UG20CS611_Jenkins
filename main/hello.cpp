15509c2911590791521b8fdadedb0ff72562199c
vinayakumaru <pes1ug20cs611@pesu.pes.edu>
2023-02-19 22:52:42 +0530

    changes made

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "PES1UG20CS611 have successfully built and run\n";
+  cout << "Changes made by PES1UG20CS611\n";
   return 0;
 }
